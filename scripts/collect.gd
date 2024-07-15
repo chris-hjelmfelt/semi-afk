1e3793521906909413a5a251bf995d50fdf0673b
Christine Hjelmfelt <christine.hjelmfelt@gmail.com>
2024-07-15 09:50:41 -0500

    changed block to tree, added sound effects

--- scripts/collect.gd
+++ scripts/collect.gd
@@ -2,21 +2,23 @@ extends Area3D
 
 @export var item1: ItemData
 @export var quantity: int = 1
-
 	
 func getItem():
 	for i in range(PlayerData.items.size()):
 		if PlayerData.items[i][0] == item1.name:
 			PlayerData.items[i][1] += quantity
-	print("Collect - " + item1.name)
 
 
 func _on_input_event(_camera, event, _position, _normal, _shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		print(str(name) + " clicked")
 		$CollectTimer.start()
-		
+		$SoundTimer.start()
 
 
-func _on_collect_timer_timeout():
+func _on_collect_timer_timeout():	
+	$SoundTimer.stop()
 	getItem()
+
+
+func _on_sound_timer_timeout():	
+	$AudioStreamPlayer.play()
--- scripts/display.gd
+++ scripts/display.gd
@@ -2,4 +2,5 @@ extends Control
 
 
 func _process(_delta):
+	$Items/VBoxContainer/Wood/Label2.text = str(PlayerData.items[0][1])
 	$Items/VBoxContainer/Fish/Label2.text = str(PlayerData.items[1][1])
